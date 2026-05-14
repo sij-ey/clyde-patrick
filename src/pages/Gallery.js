469cc07e91188213ff51e49f171e137622a6d6ed
sij-ey <charlseochieng01@gmail.com>
2026-05-14 12:15:14 +0300

    masonry

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,3 +1,5 @@
+import Masonry from "react-masonry-css";
+
 import cover1 from "../assets/cover1.jpg";
 import cover2 from "../assets/cover2.jpg";
 import cover3 from "../assets/cover3.jpg";
@@ -10,6 +12,13 @@ import cover9 from "../assets/cover9.jpg";
 import cover10 from "../assets/cover10.jpg";
 import cover11 from "../assets/cover11.jpg";
 import cover12 from "../assets/cover12.jpg";
+
+const breakpointColumns = {
+  default: 3,
+  1024: 3,
+  768: 2,
+  500: 1,
+};
 
 function Gallery() {
   const images = [
@@ -31,7 +40,7 @@ function Gallery() {
     <div className="min-h-screen bg-gray-950 text-white p-6">
       <button
         onClick={() => window.history.back()}
-        className="mb-6 px-4 py-2 bg-cyan-500 text-white rounded hover:bg-cyan-600 transition duration-300"
+        className="mb-6 px-4 py-2 bg-cyan-5oo text-white rounded hover:bg-cyan-600 transition duration-300"
       >
         Back to Home
       </button>
@@ -39,20 +48,24 @@ function Gallery() {
         Gallery
       </h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <Masonry
+        breakpointCols={breakpointColumns}
+        className="flex gap-6"
+        columnClassName="space-y-6"
+      >
         {images.map((img, index) => (
           <div
             key={index}
-            className="overflow-hidden rounded-xl shadow-lg bg-gray-900 flex items-center justify-center"
+            className="overflow-hidden rounded-xl shadow-lg bg-gray-900"
           >
             <img
               src={img}
               alt={`gallery-${index}`}
-              className="w-full h-72 object-contain hover:scale-105 transition duration-500"
+              className="w-full h-auto object-cover hover:scale-105 transition duration-500"
             />
           </div>
         ))}
-      </div>
+      </Masonry>
     </div>
   );
 }
